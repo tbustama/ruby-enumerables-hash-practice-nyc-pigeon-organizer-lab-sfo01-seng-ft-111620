47a9dc0a7f4895694e1c7bce3abbc6d5c046ba07
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-04 17:55:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  data.each do |k1, v1|
+    v1.each do |k2, v2|
+      v2.each do |ele|
+        if !hash[ele]
+          hash[ele] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        end
+        if v2.include?(ele)
+          hash[ele][k1] << k2.to_s
+        end
+      end
+    end
+  end
+  binding.pry
+  hash
 end
+
+
+
+
+# pigeon_data = {
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
